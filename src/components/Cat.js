f42a614db9930800d0827c28cdad7312c04d3e17
aliazhar <aliazhar.idx@gmail.com>
2023-12-28 18:32:02 +0700

    [ + ] First Commit

---
+++ src/components/Cat.js
@@ -0,0 +1,8 @@
+import { Avatar } from '@mui/material';
+import React from 'react';
+
+const Cat = () => {
+  return <Avatar src="https://images.unsplash.com/photo-1617854818583-09e7f077a156?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />;
+};
+
+export default Cat;
